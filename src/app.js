31440b3f9d6d5067219dc25c3966b3a1bbb4806d
John Holland <john.gebhard.holland@gmail.com>
2022-02-24 15:40:51 -0500

    initial update with react and webpack build

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+//todo:
+// * Material UI, styled components, functional component style
+// * Controls: read speed, reset speed, rotation angle, wave width
+//
+// https://medium.com/@seanlumjy/build-a-chrome-extension-that-injects-css-into-your-favourite-website-9b65f722f409
+
+class App extends React.Component {
+    render() {
+        return <h1>Hello World</h1>
+    }
+}
+export default App
