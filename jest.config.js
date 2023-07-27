d66680ffb92f77334235c391f4d79bc121a6cb97
John Holland <john.gebhard.holland@gmail.com>
2023-07-27 17:46:36 -0400

    added jest.config.js for jsdom and transform variables, and go-button snapshot

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  "transform": {
+    "node_modules/variables/.+\\.(j|t)sx?$": "ts-jest"
+  },
+  "transformIgnorePatterns": [
+    "node_modules/(?!variables/.*)"
+  ]
+};
