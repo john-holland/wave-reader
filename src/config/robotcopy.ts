b8d530abe4be6e7fc745500e3f622e13738e4264
John Holland <john.gebhard.holland@gmail.com>
2024-01-16 07:16:13 -0500

    added a copy config

--- src/config/robotcopy.ts
+++ src/config/robotcopy.ts
@@ -1,1 +1,57 @@
 //todo: implement: single configuration for delivery targets configured via configuration (webpack a plus)
+import {
+    Client,
+    ClientDiscovery,
+    ClientLocation, GoogleChromeRuntimeProxy,
+    GoogleClientMessengerService,
+    IClientMessengerService
+} from "../util/state-machine";
+import Message from "../models/message";
+
+type ClientDiscoveryConfig = {
+    up: { (): Client<any> }
+    down: { (): Client<any> }
+}
+
+type RobotCopyConfig = {
+    clients: { [key: string]: ClientDiscoveryConfig }
+}
+
+const clientCacheMap = new Map<string, Client<any>>()
+
+export const copyConfig: RobotCopyConfig = {
+    "clients": {
+        "popup": {
+            up: () => {
+                if (!clientCacheMap.has("popup")) {
+                    clientCacheMap.set("popup", new Client<Message<any>>(new GoogleClientMessengerService(
+                        {
+                            from: ClientLocation.POPUP,
+                            to: ClientLocation.CONTENT
+                        },
+                        new Map<string, IClientMessengerService<Message<any>>>(),
+                        new GoogleChromeRuntimeProxy())))
+                }
+                return clientCacheMap.get("popup")!
+            },
+            down: () => {
+                if (!clientCacheMap.has("popup")) {
+                    clientCacheMap.set("popup", new Client<Message<any>>(new GoogleClientMessengerService(
+                        {
+                            from: ClientLocation.POPUP,
+                            to: ClientLocation.CONTENT
+                        },
+                        new Map<string, IClientMessengerService<Message<any>>>(),
+                        new GoogleChromeRuntimeProxy())))
+                }
+                return clientCacheMap.get("popup")!
+            }
+        }
+    }
+}
+
+export const clientForLocation = (location: string): ClientDiscoveryConfig => {
+    if (!(location in copyConfig.clients)) throw new Error("location" + location + " not in copyConfig!");
+
+    return copyConfig.clients[location]
+}
